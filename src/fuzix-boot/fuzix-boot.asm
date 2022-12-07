84bdeb4d8bbbbdda14cb26509bf2f3894c6697d7
dominicbeesley <dominic@authorityfile.co.uk>
2022-12-07 18:41:34 +0000

    NEW: unpack a decb image at 01 0000 to 00 0000

--- src/fuzix-boot/fuzix-boot.asm
+++ src/fuzix-boot/fuzix-boot.asm
@@ -4,6 +4,23 @@
 		CODE
 		setdp	$0
 
+SRC_PAGE		RMB	2
+DEST_PAGE	RMB	2
+
+
+; DB: new fuzix boot ROM map:
+; kernel will run in bottom of RAM at pages 80,81,82,83 (init common = 83) (ChipRAM 00 0000..00 FFFF)
+; a compressed (Exomizer 2) image should be available at pages 84 to 87 (ChipRAM 01 0000..01 FFFF) i.e. 
+;  *BLLOAD E.KERNEL 10000
+
+; put the compressed image after the 1st 64K as the BLUTILS utility rom tramples on the 1st 64
+
+KERNEL_IMAGE_JIM	equ	$0100	; the JIM page for the base of the kernel image (decb format)
+KERNEL_IMAGE_MMU	equ	$84	; the starting MMU page for the kernel image (decb format)
+
+KERNEL_RUN_JIM	equ	$0000	; the JIM page for the base of the kernel at run-time
+KERNEL_RUN_MMU	equ	$80	; the starting MMU for the base of the kernel at run-time
+
 
 MACH_BEEB	equ	1
 
@@ -117,20 +134,70 @@ handle_res	clra
 		leay	-1,Y
 		bne	1B
 
+		; unpack DECB binary to raw image
 
-		; copy "bounce" code at chipram 0 onards
+		lda	#JIM_DEVNO_BLITTER
+		sta	fred_JIM_DEVNO		; enable page-wide memory access
+
+		ldx	#str_unpack
+		jsr	ser_send_strX
+
+		ldd 	#KERNEL_IMAGE_JIM
+		std	SRC_PAGE
+		ldx 	#$FD00			; X is source byte
+
+hdr_lp		; get a header from JIM
+		jsr	JimGetB
+
+		tstb
+		beq	1F
+		incb
+		beq	decb_done
+
+		ldx	#str_bad_chunk
+		jsr	ser_send_strX
+jmphere		jmp	jmphere
+
+
+1		jsr	JimGetD
+		tfr	D,Y			; Y is length
+		jsr	JimGetB			; high byte of dest
+		clra
+		addd	#KERNEL_RUN_JIM
+		std	DEST_PAGE
+		jsr	JimGetB
+		lda 	#JIM/256
+		tfr	D,U			; U is destination 
+
+2		jsr	JimGetB
+		jsr	JimSetB
+		leay	-1,Y
+		bne	2B
+		bra	hdr_lp
+
+decb_done
+
+		lda	#0
+		sta	fred_JIM_DEVNO		; enable page-wide memory access
+
+
+		; copy "bounce" code at chipram 100 onwards (we expect 0..200 to be free)
+
+		ldx	#str_boot
+		jsr	ser_send_strX
+
 
 		; set up task 0 to have SYS at top (this code), ChipRAM at ram 0-BFFF and SYS screen memory C000-BFFF
 
 		lda 	#0
 		sta	MMU_ACC_KEY
-		lda	#$84
+		lda	#KERNEL_RUN_MMU
 		sta	MMU_MAP+MMU_16_0
-		lda	#$85
+		lda	#KERNEL_RUN_MMU+1
 		sta	MMU_MAP+MMU_16_4
-		lda	#$86
+		lda	#KERNEL_RUN_MMU+2
 		sta	MMU_MAP+MMU_16_8
-		lda	#$C3
+		lda	#$C3			; this ROM
 		sta	MMU_MAP+MMU_16_C
 
 		lda 	#MMU_CTL_ENMMU
@@ -158,7 +225,7 @@ ut0_r
 ut0		; this is the "bounce" task that is copied to 
 
 		; map in top page of RAM in supervisor task 
-		lda	#$87
+		lda	#KERNEL_RUN_MMU+3
 		sta	MMU_MAP+MMU_16_C
 
 		; call the kernel
@@ -195,6 +262,31 @@ ser_send_strX	pshs	A
 		bra	1B
 2		puls	A,PC
 
+
+HEX4		pshs	A
+		bsr	HEX2
+		tfr	B,A
+		bsr	HEX2
+		puls	A,PC
+
+HEX2		sta	,-S
+		lsra
+		lsra
+		lsra
+		lsra
+		bsr	HEX1
+		lda	,S
+		bsr	HEX1
+		puls	A,PC
+
+
+HEX1		anda	#$0F
+		adda	#'0'
+		cmpa	#'9'
+		bls	1F
+		adda	#'A'-'9'-1
+1		; drop thru to print		
+
 ser_send_A	stb	,-S
 		ldb 	#ACIA_TDRE
 1		bitb	sheila_ACIA_CTL
@@ -203,11 +295,43 @@ ser_send_A	stb	,-S
 		puls	B,PC
 
 
+JimGetD		bsr	JimGetB
+		tfr	B,A
+		; fall through
+JimGetB		sta	,-S
+		ldd	SRC_PAGE
+		std	fred_JIM_PAGE_HI
+		ldb	,X+
+		cmpx	#JIM+$100
+		blo	1F
+		ldx 	#JIM
+		; gone over page boundary
+		lda	#'.'
+		jsr	ser_send_A		; tell the world
+		inc	SRC_PAGE+1
+		bne	1F
+		inc	SRC_PAGE
+1		puls	A,PC
+
+JimSetB		std	,--S
+		ldd	DEST_PAGE
+		std	fred_JIM_PAGE_HI
+		ldb	1,S
+		stb	,U+
+		cmpu	#JIM+$100
+		blo	1F
+		ldu	#JIM
+		; gone over page boundary
+		inc	DEST_PAGE+1
+		bne	1F
+		inc	DEST_PAGE
+1		puls	D,PC
+
 
 str_init		FCB	"Fuzix Boot Rom for Blitter+SBC09",13,10,13,10,0
-str_map		FCB	"Setting memory map to:", 13,10
-		FCB	" Task 0 = 80,81,82,83", 13, 10, 0
-str_boot		FCB	"Starting image at 00 0200",13,10,13,10
+str_unpack	FCB	"Unpacking image at 01 0000 to 00 0000",13,10,0
+str_boot		FCB	13,10,13,10,"Starting image at 00 0200",13,10,13,10,0
+str_bad_chunk	FCB	"Bad Chunk header found", 13,10,0
 
 mode_7_setup  	FCB	$3F, $28, $33, $24, $1E, $02, $19, $1C
 		FCB	$93, $12, $72, $13, $28, $00, $00, $00
